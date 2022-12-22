a772abf42dab1ffff999045451c746a7b3d06a83
Phuntsho806 <pwangmo328@gmail.com>
2022-12-22 10:59:50 +0600

    Add

---
+++ first.py
@@ -0,0 +1,25 @@
+
+#print('*' * 10)
+'''name='John Smith'
+age= 26
+is_new = True
+name = input('What is your name? ')
+fav_color = input('what is your favourite color? ')
+print(name + ' likes ' + fav_color)
+birth_year = input('Birth year: ')
+age = 2022 - int(birth_year)
+print(age)
+
+weight = input('Weight (lbs): ')
+weight_kg = int(weight) * 0.45
+print(weight_kg) '''
+
+course = 'Hi John'
+print(course[1:-1])
+#formatted string
+first = 'John'
+last = 'Smith'
+msg = f'{first} [{last}] is a coder'
+print(msg)
+print(len(course))
+print(course.upper())
